bd71a6acdc7c3485ffe94c6d86e5aa4b89f3acd3
scbq <felipe.aguileraf@mayor.cl>
2024-07-10 20:21:05 -0400

    commit inicial

---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+$(window).scroll(function() {
+    if ($(this).scrollTop() > 50) {
+        $('.navbar').addClass('scrolled');
+    } else {
+        $('.navbar').removeClass('scrolled');
+    }
+});
